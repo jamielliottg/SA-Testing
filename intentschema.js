26979dc51dc4448f8bc0a380bbb678da3e2ee8ad
Jamie G <jamiegrossman@outlook.com>
2017-08-28 05:21:59 +0100

    Create intentschema.js

---
+++ intentschema.js
@@ -0,0 +1,19 @@
+{
+  "intents": [
+    {
+      "intent": "AMAZON.YesIntent"
+    },
+    {
+      "intent": "numberIntent",
+      "slots": [
+        {
+          "type": "AMAZON.Number",
+          "name": "numberVal"
+        }
+      ]
+    },
+    {
+      "intent": "AMAZON.StopIntent"
+    }
+  ]
+}
